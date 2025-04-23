0e1cacb47e5e9e819146490a268f490c63cedde8
TayfunOgz <tayfunouz@gmail.com>
2025-04-23 10:49:32 +0300

    Update scores.py

--- scores.py
+++ scores.py
@@ -75,8 +75,8 @@ def get_scores():
 
 if __name__ == "__main__":
     create_database()
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=True, allow_unsafe_werkzeug=True)
+   import os
+port = int(os.environ.get("PORT", 5000))
+socketio.run(app, host="0.0.0.0", port=port)
    
 
